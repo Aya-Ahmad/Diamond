d56f7954c4870a7ba3aff6f8e07b985dfec39f0b
Aya Ahmad <aya.w.ahmadd@gmail.com>
2024-10-30 12:55:59 -0700

    Update diamond.py

--- diamond.py
+++ diamond.py
@@ -1,8 +1,8 @@
 rows = int ( input ("Enter a number of rows: "))
 character  = "* "
-for i in range ( 1, rows +1 ):
+for i in range ( 1, rows +1 ):  #Creating a for loop to print the top half of the palindrome
     print ( " " * ( rows - i ) + character * ( i ) )
     
-for j in range( rows-1, 0, -1):
+for j in range( rows-1, 0, -1 ): #Creating another for loop to print the bottom half of the palindrome
      print ( " " * ( rows - j ) + character * ( j ) )
     
